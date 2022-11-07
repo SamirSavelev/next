67486e6ca29f5ed5bd94a36743aea130ad702e52
Михаил Савельев <michaeloriginal@yandex.ru>
2022-11-07 13:02:19 +0300

    Start a project

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+  render() {
+    return (
+      <Html lang="ru">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="description" content="TakeOff Stuff" />
+          <link rel="icon" href="/logo.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;800&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { withLayout } from "../layout/Layout";
+
+const Home: NextPage = () => {
+  return <>\f</>;
+};
+
+export default withLayout(Home);
